255a326c724bce829835699b97d093ac121f5df3
Peter Reutemann <fracpete@gmail.com>
2025-06-27 15:47:33 +1200

    no longer pinning setuptools to <81, requiring seppl 0.2.17 now

--- setup.py
+++ setup.py
@@ -30,8 +30,8 @@ setup(
     },
     packages=find_namespace_packages(where='src'),
     install_requires=[
-        "setuptools<81",
-        "seppl>=0.2.16",
+        "setuptools",
+        "seppl>=0.2.17",
         "wai_logging",
         "wai_common>=0.0.45",
         "wai_spectralio>=0.0.3",
