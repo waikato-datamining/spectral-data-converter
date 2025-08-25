0d6ebf0bae165ec70682fa42257e86281322f81d
Peter Reutemann <fracpete@gmail.com>
2025-08-25 12:59:41 +1200

    DiscardNegatives moved from kasperl into respective sub-projects

--- src/sdc/class_lister_ignored.py
+++ src/sdc/class_lister_ignored.py
@@ -5,6 +5,5 @@ from typing import List, Dict
 def list_classes() -> Dict[str, List[str]]:
     return {
         "seppl.io.Filter": [
-            "kasperl.filter.DiscardNegatives",
         ],
     }
