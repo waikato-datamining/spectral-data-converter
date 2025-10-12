494de3f994b6f4e9f392a660d27e83275d5a12cc
Peter Reutemann <fracpete@gmail.com>
2025-10-13 11:45:55 +1300

    fixed imports for PollDir/WatchDir readers

--- src/sdc/reader/__init__.py
+++ src/sdc/reader/__init__.py
@@ -1,3 +1,5 @@
+from kasperl.reader import POLL_ACTIONS, POLL_ACTION_NOTHING, POLL_ACTION_MOVE, POLL_ACTION_DELETE
+from kasperl.reader import EVENTS, EVENT_MODIFIED, EVENT_CREATED, WATCH_ACTIONS, WATCH_ACTION_NOTHING, WATCH_ACTION_MOVE, WATCH_ACTION_DELETE, POLLING_TYPES, POLLING_TYPE_NEVER, POLLING_TYPE_INITIAL, POLLING_TYPE_ALWAYS
 from ._adams import AdamsReader, ReportSampleDataReader
 from ._arff import ARFFReader
 from ._asc import ASCReader
@@ -11,8 +13,8 @@ from ._multi import MultiReader
 from ._nir import NIRReader
 from ._opus import OPUSReader
 from ._opus_ext import OPUSExtReader
-from ._poll_dir import PollDir, POLL_ACTIONS, POLL_ACTION_NOTHING, POLL_ACTION_MOVE, POLL_ACTION_DELETE
+from ._poll_dir import PollDir
 from ._pyfunc import PythonFunctionReader
 from ._spa import SPAReader
 from ._zip import ZipReader
-from ._watch_dir import WatchDir, EVENTS, EVENT_MODIFIED, EVENT_CREATED, WATCH_ACTIONS, WATCH_ACTION_NOTHING, WATCH_ACTION_MOVE, WATCH_ACTION_DELETE, POLLING_TYPES, POLLING_TYPE_NEVER, POLLING_TYPE_INITIAL, POLLING_TYPE_ALWAYS
+from ._watch_dir import WatchDir
